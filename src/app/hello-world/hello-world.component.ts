21fae3b4613ac9cc7742158ac4236467980f44b7
ShenGuangLong <2433221931@qq.com>
2024-06-01 02:09:23 +0800

    Initial commit

---
+++ src/app/hello-world/hello-world.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-world',
+  standalone: true,
+  imports: [],
+  templateUrl: './hello-world.component.html',
+  styleUrls: ['./hello-world.component.css']
+})
+export class HelloWorldComponent implements OnInit {
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+}
